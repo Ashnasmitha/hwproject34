dcf52243a899eee7ba270e8d69a2ab80e9ad085b
Ashnasmitha <75996347+Ashnasmitha@users.noreply.github.com>
2021-01-27 21:17:26 +0530

    Add files via upload

---
+++ Hero.js
@@ -0,0 +1,25 @@
+class Hero{
+    constructor(x,y,width,height){
+        var options={
+            frictionAir:1,
+            density:40
+        }
+        this.x=x;
+        this.y=y;
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.image=loadImage("images/Superhero.png");
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+        var angle=this.body.angle;
+        push ();
+        translate (pos.x,pos.y);
+        rotate (angle);
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+        pop ();
+
+
+    }
+}
---
+++ Monster.js
@@ -0,0 +1,18 @@
+class Monster{
+    constructor(x,y,width,height){
+        var options={
+         isStatic:true
+        }
+        this.body=Bodies.rectangle(x,y,width,height,options);
+        this.width=width;
+        this.height=height;
+        this.image=loadImage("images/Monster-01.png");
+        World.add(world,this.body);
+    }
+    display(){
+        imageMode(CENTER);
+        image(this.image,0,0,this.width,this.height);
+
+
+    }
+}
